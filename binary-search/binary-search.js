ed0c532fc1a6ccb9018ac840f825144b997e7c26
Sara Parelhoff <sara.parelhoff@gmail.com>
2019-01-28 14:49:56 -0500

    work so far

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,57 @@
-'use strict';
+"use strict";
+
+const binarySearch = (array, target) => {
+  if (!array.length) return false;
+
+  if (array.length === 1) {
+    if (array[0] === target) {
+      return true;
+    } else return false;
+  }
+
+  let front = 0;
+  let end = array.length - 1;
+  let middle = Math.ceil(array.length / 2);
+
+  while (front !== middle && end !== middle) {
+    if (target === array[middle]) return true;
+    else if (target > array[middle]) {
+      front = middle;
+    } else {
+      end = middle;
+	}
+	middle = Math.ceil((end - front) / 2) + front;
+  }
+
+  if (target === array[front] || target === array[end]) return true;
+
+  return false;
+};
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
-};
+// const binarySearch = (array, target) => {
+
+// 	if (!array.length) return false
+
+// 	if (array.length === 1) {
+// 		if (target === array[0]) return true
+// 		else return false
+// 	}
+
+// 	const midpoint = Math.floor(array.length / 2)
+// 	const frontArray = array.slice(0, midpoint)
+// 	const backArray = array.slice(midpoint)
+
+// 	if ( target === array[midpoint]) return true
+
+// 	else if (target > array[midpoint]) {
+// 		return binarySearch(backArray, target)
+// 	}
+
+// 	else {
+// 		return binarySearch(frontArray, target)
+// 	}
+// };
 
 /*
 	EXTRA CREDIT:
@@ -14,4 +62,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
